ba19b1a5182f3b9ac64a832e564dce1ced8a45ac
dk10119 <tranduclong092@gmail.com>
2024-05-01 12:15:41 +0300

    Feat: display navlink to the new calculator route and display the post data from the form

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class CalculatorController extends AbstractController
-{
-    #[Route('/calculator', name: 'app_calculator')]
-    public function index(): Response
-    {
-        return $this->render('calculator/index.html.twig', [
+class CalculatorController extends AbstractController {
+    #[Route('/calculator', name: 'calculator')]
+    public function calculator(Request $req): Response {
+        return $this->render('calculator/calculator.html.twig', [
             'controller_name' => 'CalculatorController',
         ]);
+    }
+
+    #[Route('/calculate', name: 'app_calculate')]
+    public function calculate(Request $req): Response {
+        $num1 = $req->get("first_number");
+        $num2 = $req->get("second_number");
+        $operator = $req->get("operator");
+        exit("$num1, $num2, $operator");
     }
 }
